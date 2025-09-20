2d8915b66561e865df9c5341a6b230c14673c5cf
Zac Swider <zac.swider@gmail.com>
2025-09-19 17:24:24 -0700

    initial init.lua file

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- this file opened every time nvim is started, so any options or configurations go here
+
+-- enable line numbers
+-- `vim` is a global variable. It is a lua table that is an interface to interact with nvim functionalities
+-- `wo` is the "Windows Options" field within the `vim` table
+-- `number` is the options to display line numbers
+vim.wo.number = true
