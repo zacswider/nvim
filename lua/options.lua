0d1d7bde1fcd91b3a145dc7e3d7a8bb6b3e8360a
Zac Swider <zac.swider@gmail.com>
2025-09-19 19:03:01 -0700

    clone kickstarter.nvim

--- lua/options.lua
+++
@@ -1,9 +0,0 @@
-
--- this file opened every time nvim is loaded because init.lua is opened every time nvim is loaded and it imports
--- this file with the line `require 'options'`
-
--- enable line numbers
--- `vim` is a global variable. It is a lua table that is an interface to interact with nvim functionalities
--- `wo` is the "Windows Options" field within the `vim` table
--- `number` is the options to display line numbers
-vim.wo.number = true
