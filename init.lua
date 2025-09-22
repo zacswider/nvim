fdbf0500151dfce9512fbbbb04908b4414ce14c0
Zac Swider <51267747+zacswider@users.noreply.github.com>
2025-09-22 15:40:10 -0700

    Starting config with basic functionality (#1)
    Lots of plugins and functionality sniped from https://github.com/nvim-lua/kickstart.nvim and https://github.com/hendrikmi/neovim-kickstart-config.

--- init.lua
+++ init.lua
@@ -1,7 +1,40 @@
--- this file opened every time nvim is started, so any options or configurations go here
+require 'core.options'
+require 'core.keymaps'
 
--- enable line numbers
--- `vim` is a global variable. It is a lua table that is an interface to interact with nvim functionalities
--- `wo` is the "Windows Options" field within the `vim` table
--- `number` is the options to display line numbers
-vim.wo.number = true
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  You can press `?` in this menu for help.
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup {
+  require 'plugins.neotree',
+  require 'plugins.colortheme',
+  require 'plugins.bufferline',
+  require 'plugins.lualine',
+  require 'plugins.treesitter',
+  require 'plugins.telescope',
+  require 'plugins.lsp',
+  require 'plugins.autocompletion',
+  require 'plugins.autoformatting',
+  require 'plugins.gitsigns',
+  require 'plugins.alpha',
+  require 'plugins.indentblankline',
+  require 'plugins.misc',
+}
