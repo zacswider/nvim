97b0ebb52a41aa850156e679bb1eb68e8252c2e1
Zac Swider <zac.swider@gmail.com>
2025-09-22 13:01:18 -0700

    import new plugins

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
-end
+end 
 
 ---@type vim.Option
 local rtp = vim.opt.rtp
@@ -28,5 +28,8 @@ require('lazy').setup({
     require 'plugins.colortheme',
     require 'plugins.bufferline',
     require 'plugins.lualine',
+    require 'plugins.treesitter',
+    require 'plugins.telescope',
+    require 'plugins.lsp',
 })
 
