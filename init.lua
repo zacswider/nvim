8b46dd4da6668fdb47074dc0fa6453a615cc26c9
Zac Swider <zac.swider@gmail.com>
2025-09-22 13:22:10 -0700

    format everything

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
-require 'core.options'
-require 'core.keymaps'
+require("core.options")
+require("core.keymaps")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end 
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
 
 ---@type vim.Option
 local rtp = vim.opt.rtp
@@ -23,13 +23,14 @@ rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
-    require 'plugins.neotree',
-    require 'plugins.colortheme',
-    require 'plugins.bufferline',
-    require 'plugins.lualine',
-    require 'plugins.treesitter',
-    require 'plugins.telescope',
-    require 'plugins.lsp',
+require("lazy").setup({
+	require("plugins.neotree"),
+	require("plugins.colortheme"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformatting"),
 })
-
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,16 +1,16 @@
 vim.wo.number = true -- Show line numbers
-vim.o.relativenumber = true  -- Show relative line numbers
-vim.o.clipboard = 'unnamedplus'  -- Sync clipboard between OS and nvim
-vim.o.wrap = false  -- Don't wrap long lines
-vim.o.linebreak = true  -- Don't split works; companion to wrap
-vim.o.mouse = 'a' -- Enable mouse mode
+vim.o.relativenumber = true -- Show relative line numbers
+vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and nvim
+vim.o.wrap = false -- Don't wrap long lines
+vim.o.linebreak = true -- Don't split works; companion to wrap
+vim.o.mouse = "a" -- Enable mouse mode
 vim.o.autoindent = true -- Keep the indentation of the previous line
 vim.o.ignorecase = true -- Case-insensitive search unless \C or capital letter in search
 vim.o.smartcase = true
 vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore (default: true) b/c lualine handles this
 
-vim.o.shiftwidth = 4  -- Number of spaces inserted per indentation
-vim.o.tabstop = 4  -- Number of spaces inserted per tab
+vim.o.shiftwidth = 4 -- Number of spaces inserted per indentation
+vim.o.tabstop = 4 -- Number of spaces inserted per tab
 vim.o.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
 vim.o.expandtab = true -- Convert tabs to spaces (default: false)
 
@@ -29,10 +29,9 @@ vim.o.backup = false -- Creates a backup file (default: false)
 vim.o.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
 vim.o.undofile = true -- Save undo history (default: false)
 
-vim.o.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience (default: 'menu,preview')
-vim.opt.shortmess:append 'c' -- Don't give |ins-completion-menu| messages (default: does not include 'c')
-vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default: does not include '-')
-vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
+vim.o.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience (default: 'menu,preview')
+vim.opt.shortmess:append("c") -- Don't give |ins-completion-menu| messages (default: does not include 'c')
+vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (default: does not include '-')
+vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 
-vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
-
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
