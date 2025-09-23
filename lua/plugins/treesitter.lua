7d6ed2e474fba69aabbae7d81b757a5c1a16ecb0
Zac Swider <zac.swider@gmail.com>
2025-09-23 14:18:03 -0700

    update treesitter to understand blocks

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -43,6 +43,41 @@ return { -- Highlight, edit, and navigate code
     },
     indent = { enable = true, disable = { 'ruby' } },
   },
+  textobjects = {
+    move = {
+      enable = true,
+      set_jumps = false, -- you can change this if you want.
+      goto_next_start = {
+        --- ... other keymaps
+        [']b'] = { query = '@code_cell.inner', desc = 'next code block' },
+      },
+      goto_previous_start = {
+        --- ... other keymaps
+        ['[b'] = { query = '@code_cell.inner', desc = 'previous code block' },
+      },
+    },
+    select = {
+      enable = true,
+      lookahead = true, -- you can change this if you want
+      keymaps = {
+        --- ... other keymaps
+        ['ib'] = { query = '@code_cell.inner', desc = 'in block' },
+        ['ab'] = { query = '@code_cell.outer', desc = 'around block' },
+      },
+    },
+    swap = { -- Swap only works with code blocks that are under the same
+      -- markdown header
+      enable = true,
+      swap_next = {
+        --- ... other keymap
+        ['<leader>sbl'] = '@code_cell.outer',
+      },
+      swap_previous = {
+        --- ... other keymap
+        ['<leader>sbh'] = '@code_cell.outer',
+      },
+    },
+  },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --
