8eab44858fe59c85be56ed03e6219fb6383184d6
Zac Swider <zac.swider@gmail.com>
2025-09-23 16:34:16 -0700

    switch to vscode dark theme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,26 +1,53 @@
 return {
-  'shaunsingh/nord.nvim',
-  laze = false,
+  'Mofiqul/vscode.nvim',
+  lazy = false,
   priority = 1000,
   config = function()
-    -- Example config in lua
-    vim.g.nord_contrast = true
-    vim.g.nord_borders = false
-    vim.g.nord_disable_background = true
-    vim.g.nord_italic = false
-    vim.g.nord_uniform_diff_background = true
-    vim.g.nord_bold = false
-
-    -- Load the colorscheme
-    require('nord').set()
-
+    -- Set dark theme by default
+    vim.o.background = 'dark'
+    
     -- Toggle background transparency
     local bg_transparent = true
 
+    local c = require('vscode.colors').get_colors()
+    
+    local function setup_vscode()
+      require('vscode').setup({
+        -- Enable transparent background
+        transparent = bg_transparent,
+        
+        -- Enable italic comment
+        italic_comments = true,
+        
+        -- Enable italic inlay type hints
+        italic_inlayhints = true,
+        
+        -- Underline links
+        underline_links = true,
+        
+        -- Disable nvim-tree background color when transparent
+        disable_nvimtree_bg = bg_transparent,
+        
+        -- Apply theme colors to terminal
+        terminal_colors = true,
+        
+        -- Override colors if needed
+        color_overrides = {},
+        
+        -- Override highlight groups if needed
+        group_overrides = {}
+      })
+      
+      -- Load the colorscheme
+      vim.cmd.colorscheme "vscode"
+    end
+
+    -- Initial setup
+    setup_vscode()
+
     local toggle_transparency = function()
       bg_transparent = not bg_transparent
-      vim.g.nord_disable_background = bg_transparent
-      vim.cmd [[ colorscheme nord ]]
+      setup_vscode()
     end
 
     vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
