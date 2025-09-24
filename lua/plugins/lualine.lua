b2c6615c8fa91f22f8e8ae28bb222b5b366edcaa
Zac Swider <zac.swider@gmail.com>
2025-09-24 09:59:56 -0700

    update lualine colorscheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -40,7 +40,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'nord', -- Set theme based on environment variable
+        theme = 'vscode', -- Updated to match new colorscheme
         -- Some useful glyphs:
         -- https://www.nerdfonts.com/cheat-sheet
         --        
