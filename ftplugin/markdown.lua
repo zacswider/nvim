8015eba1c4022ee44140f8a54482b08c0e4865e3
Zac Swider <zac.swider@gmail.com>
2025-09-24 12:01:24 -0700

    agent changes for markdown styling and text settings

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,1 +1,7 @@
 require('quarto').activate()
+
+-- Adjust text width for notebooks
+vim.opt_local.textwidth = 100 -- Wider text width for notebooks
+vim.opt_local.colorcolumn = '100' -- Show column guide
+vim.opt_local.wrap = true -- Enable line wrapping
+vim.opt_local.linebreak = true -- Break at word boundaries
