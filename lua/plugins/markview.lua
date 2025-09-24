aab011bd0cdaa3bb8e07ee9d05586057b5aaf4fe
Zac Swider <zac.swider@gmail.com>
2025-09-24 12:01:59 -0700

    agent changes for inline code

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -10,4 +10,46 @@ return {
   dependencies = {
     'saghen/blink.cmp',
   },
+
+  opts = {
+    code_blocks = {
+      style = 'language',
+      position = nil,
+      min_width = 100, -- Minimum width for code blocks
+      pad_amount = 0, -- Padding around code blocks
+      pad_char = ' ', -- Character used for padding
+
+      language_direction = 'right',
+      sign = true,
+      sign_hl = nil,
+
+      -- Language names
+      language_names = {
+        python = 'Python',
+        rust = 'Rust',
+        javascript = 'JavaScript',
+        typescript = 'TypeScript',
+      },
+    },
+
+    -- Configure inline code
+    inline_codes = {
+      enable = true,
+      corner_left = '',
+      corner_right = '',
+      padding_left = ' ',
+      padding_right = ' ',
+    },
+
+    -- Fix: Use markdown → headings instead of deprecated headings
+    markdown = {
+      headings = {
+        enable = true,
+        shift_width = 0,
+        heading_1 = { style = 'icon' },
+        heading_2 = { style = 'icon' },
+        heading_3 = { style = 'icon' },
+      },
+    },
+  },
 }
