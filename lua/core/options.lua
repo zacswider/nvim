dae83d201aed9c02ecce082e0a1006143c418771
Zac Swider <zac.swider@gmail.com>
2025-10-01 08:25:21 -0700

    show cwd and buffname in tab title

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -36,3 +36,6 @@ vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
+
+vim.opt.title = true -- Enable tab title
+vim.opt.titlestring = [[nvim: %{fnamemodify(getcwd(), ':t')} (%{empty(bufname('%')) ? 'No Name' : expand('%:t')})]]
