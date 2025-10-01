4f7c8f80e8c9892c56beaed9d738dbde233dcf87
Zac Swider <zac.swider@gmail.com>
2025-10-01 13:01:59 -0700

    update sidekick config

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -1,13 +1,37 @@
 return {
   'folke/sidekick.nvim',
   opts = {
+    -- add any options here
     cli = {
       mux = {
+        backend = 'zellij',
         enabled = false,
       },
     },
   },
   keys = {
+    {
+      '<tab>',
+      function()
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if require('sidekick').nes_jump_or_apply() then
+          return -- jumped or applied
+        end
+
+        -- if you are using Neovim's native inline completions
+        if vim.lsp.inline_completion.get() then
+          return
+        end
+
+        -- any other things (like snippets) you want to do on <tab> go here.
+
+        -- fall back to normal tab
+        return '<tab>'
+      end,
+      mode = { 'i', 'n' },
+      expr = true,
+      desc = 'Goto/Apply Next Edit Suggestion',
+    },
     {
       '<c-.>',
       function()
@@ -19,9 +43,19 @@ return {
     {
       '<leader>aa',
       function()
-        require('sidekick.cli').toggle { focus = true }
+        require('sidekick.cli').toggle()
       end,
       desc = 'Sidekick Toggle CLI',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>as',
+      function()
+        require('sidekick.cli').select()
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+      end,
+      desc = 'Sidekick Select CLI',
       mode = { 'n', 'v' },
     },
     {
