1e54c3ff79be3054409c797ca0204b7b095ee6b2
Zac Swider <zac.swider@gmail.com>
2025-12-12 16:00:16 -0500

    default to opaque colortheme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -5,41 +5,41 @@ return {
   config = function()
     -- Set dark theme by default
     vim.o.background = 'dark'
-    
+
     -- Toggle background transparency
-    local bg_transparent = true
+    local bg_transparent = false
 
     local c = require('vscode.colors').get_colors()
-    
+
     local function setup_vscode()
-      require('vscode').setup({
+      require('vscode').setup {
         -- Enable transparent background
         transparent = bg_transparent,
-        
+
         -- Enable italic comment
         italic_comments = true,
-        
+
         -- Enable italic inlay type hints
         italic_inlayhints = true,
-        
+
         -- Underline links
         underline_links = true,
-        
+
         -- Disable nvim-tree background color when transparent
         disable_nvimtree_bg = bg_transparent,
-        
+
         -- Apply theme colors to terminal
         terminal_colors = true,
-        
+
         -- Override colors if needed
         color_overrides = {},
-        
+
         -- Override highlight groups if needed
-        group_overrides = {}
-      })
-      
+        group_overrides = {},
+      }
+
       -- Load the colorscheme
-      vim.cmd.colorscheme "vscode"
+      vim.cmd.colorscheme 'vscode'
     end
 
     -- Initial setup
