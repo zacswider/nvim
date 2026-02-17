43f97682acf29ad744c6f08aa0c028a6b36605c3
Zac Swider <zac.swider@gmail.com>
2026-02-17 12:26:23 -0800

    disable auto-text wrapping in markdown files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,7 +1,8 @@
 require('quarto').activate()
 
 -- Adjust text width for notebooks
-vim.opt_local.textwidth = 100 -- Wider text width for notebooks
+vim.opt_local.textwidth = 0 -- Disable auto hard-wrap while typing
 vim.opt_local.colorcolumn = '100' -- Show column guide
 vim.opt_local.wrap = true -- Enable line wrapping
 vim.opt_local.linebreak = true -- Break at word boundaries
+vim.opt_local.formatoptions:remove { 't' } -- No auto text wrapping in insert
