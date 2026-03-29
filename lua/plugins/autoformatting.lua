6d65f236b5eee08d0c4d0da32d6f0ad6e9bc4113
Zac Swider <zac.swider@gmail.com>
2026-03-29 12:46:08 -0700

    run full biome unsafe formatting on save for js/ts/tsx files

--- lua/plugins/autoformatting.lua
+++ lua/plugins/autoformatting.lua
@@ -8,6 +8,64 @@ return {
     local null_ls = require 'null-ls'
     local formatting = null_ls.builtins.formatting -- to setup formatters
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+    local biome_filetypes = {
+      javascript = true,
+      javascriptreact = true,
+      typescript = true,
+      typescriptreact = true,
+    }
+
+    local function is_biome_filetype(filetype)
+      return biome_filetypes[filetype] == true
+    end
+
+    local function biome_root(bufnr)
+      local path = vim.api.nvim_buf_get_name(bufnr)
+      if path == '' then
+        return nil
+      end
+
+      return vim.fs.root(path, { 'biome.json', 'biome.jsonc', 'package.json', '.git' })
+    end
+
+    local function biome_check_current_file(bufnr)
+      local path = vim.api.nvim_buf_get_name(bufnr)
+      if path == '' then
+        return
+      end
+
+      local root = biome_root(bufnr)
+      if not root then
+        return
+      end
+
+      local result = vim.system({
+        'bunx',
+        'biome',
+        'check',
+        '--write',
+        '--unsafe',
+        '--no-errors-on-unmatched',
+        '--files-ignore-unknown=true',
+        path,
+      }, { cwd = root }):wait()
+
+      if result.code ~= 0 then
+        vim.schedule(function()
+          vim.notify(result.stderr ~= '' and result.stderr or 'Biome check reported issues on save', vim.log.levels.WARN)
+        end)
+      end
+
+      if vim.api.nvim_buf_is_valid(bufnr) then
+        vim.schedule(function()
+          if vim.api.nvim_buf_is_valid(bufnr) then
+            vim.api.nvim_buf_call(bufnr, function()
+              vim.cmd 'checktime'
+            end)
+          end
+        end)
+      end
+    end
 
     -- Formatters & linters for mason to install
     require('mason-null-ls').setup {
@@ -39,7 +97,6 @@ return {
     local sources = {
       diagnostics.checkmake,
       formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
-      formatting.biome.with { filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' } },
       formatting.stylua,
       formatting.shfmt.with { args = { '-i', '4' } },
       formatting.terraform_fmt,
@@ -54,7 +111,7 @@ return {
       sources = sources,
       -- you can reuse a shared lspconfig on_attach callback here
       on_attach = function(client, bufnr)
-        if client:supports_method 'textDocument/formatting' then
+        if client:supports_method 'textDocument/formatting' and not is_biome_filetype(vim.bo[bufnr].filetype) then
           vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
           vim.api.nvim_create_autocmd('BufWritePre', {
             group = augroup,
@@ -66,5 +123,14 @@ return {
         end
       end,
     }
+
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      group = vim.api.nvim_create_augroup('BiomeCheckOnSave', { clear = true }),
+      callback = function(args)
+        if is_biome_filetype(vim.bo[args.buf].filetype) then
+          biome_check_current_file(args.buf)
+        end
+      end,
+    })
   end,
 }
